4f2d82828dff8b25f945ce4d0fa8ca2d700b8a7c
AlfJallasi <oneframeghan@gmail.com>
2026-08-01 10:23:54 -0400

    test: probando hook local

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import sqlite3
 app = Flask(__name__)
 
 # FALLA 1: Credencial o Token Hardcodeado (Secret Scanning)
-AWS_SECRET_KEY_SIMULATED = "AKIAIOSFODNN7EXAMPLE_SECRET_KEY"
+API_KEY = "AKIAIOSFODNN7EXAMPLE"
 
 @app.route("/buscar")
 def buscar():
